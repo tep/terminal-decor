39d4580b68ebbb3b350d3fb01812e7e6971066a4
Tim Peoples <tim@toolman.org>
2023-02-25 15:30:09 -0800

    Add/Update unit tests

--- decorator_test.go
+++ decorator_test.go
@@ -4,13 +4,10 @@ package decor
 
 import (
 	"testing"
-
-	"toolman.org/terminal/decor/internal/item"
 )
 
 func TestDecorator(t *testing.T) {
-	item.ResetID()
-	d, err := Load("xterm-256color")
+	d, err := xterm256Decorator()
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -26,31 +23,5 @@ func TestDecorator(t *testing.T) {
 		t.Errorf("Format(%q) == (%q, %v); Wanted (%q, nil)", input, got, err, want)
 	} else {
 		t.Logf("Got: %q", got)
-	}
-}
-
-func TestTemplate(t *testing.T) {
-	item.ResetID()
-	d, err := Load("xterm-256color")
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	// d.debug = 2
-	input := "@F(Grey37)[${Glyph}:@I${Key}@i]@f"
-	want := `001:START:FGCOLOR:"Grey37" + 002:TEXT:"[" + 003:VAR:"Glyph" + 004:TEXT:":" + 005:START:ITALIC + 006:VAR:"Key" + 007:STOP:ITALIC + 008:TEXT:"]" + 009:STOP:FGCOLOR`
-
-	var tmpl *Template
-	if tmpl, err = d.Template(input); err != nil || tmpl.String() != want {
-		t.Fatalf("d.Template(%q) == (%q, %v);\nWanted (%q, nil)", input, tmpl, err, want)
-	}
-
-	vars := map[string]string{"Glyph": "@F<Orange1>Ж@f", "Key": "@F{204}ABC@f"}
-	want = "\x1b[38;5;59m[\x1b[38;5;214mЖ\x1b[38;5;59m:\x1b[3m\x1b[38;5;204mABC\x1b[38;5;59m]\x1b(B\x1b[m"
-
-	if got, err := tmpl.Format(vars); err != nil || got != want {
-		t.Errorf("tmpl.Format(%#v) == (%q, %v); Wanted (%q, nil)", vars, got, err, want)
-	} else {
-		t.Logf("OUT: %q", got)
 	}
 }
---
+++ template_test.go
@@ -0,0 +1,49 @@
+// Copyright © 2023 Timothy E. Peoples
+
+package decor
+
+import (
+	"testing"
+
+	"toolman.org/terminal/decor/internal/item"
+	"toolman.org/terminal/decor/internal/series"
+)
+
+func TestTemplate(t *testing.T) {
+	d, err := xterm256Decorator()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	input := "@F(Grey37)[${Glyph}:@I${Key}@i]@f"
+
+	tmplWant := series.Build(
+		item.FGColorItem("Grey37"),
+		item.TextItem("["),
+		item.VarItem("Glyph"),
+		item.TextItem(":"),
+		item.StartItem(item.ITALIC),
+		item.VarItem("Key"),
+		item.StopItem(item.ITALIC),
+		item.TextItem("]"),
+		item.StopItem(item.FGCOLOR),
+	)
+
+	var tmpl *Template
+	if tmpl, err = d.Template(input); err != nil || !tmpl.equal(tmplWant) {
+		t.Fatalf("d.Template(%q) == (%q, %v);\nWanted (%s, nil)", input, tmpl.ss, err, tmplWant)
+	}
+
+	vars := map[string]string{"Glyph": "@F<Orange1>Ж@f", "Key": "@F{204}ABC@f"}
+	want := "\x1b[38;5;59m[\x1b[38;5;214mЖ\x1b[38;5;59m:\x1b[3m\x1b[38;5;204mABC\x1b[38;5;59m]\x1b(B\x1b[m"
+
+	if got, err := tmpl.Format(vars); err != nil || got != want {
+		t.Errorf("tmpl.Format(%#v) == (%q, %v); Wanted (%q, nil)", vars, got, err, want)
+	} else {
+		t.Logf("OUT: %q", got)
+	}
+}
+
+func (t *Template) equal(os *series.Series) bool {
+	return t.ss.Equal(os)
+}
